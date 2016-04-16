58dc44538c9b3e1bcf8e1cead2977ab172581213
Will Moritz <will@crowdfavorite.com>
2016-04-16 10:01:23 -0400

    added clientid and client secret

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 var express = require('express');
 
+CLIENT_ID = 'amzn1.application-oa2-client.b4410251e2674ae09b17315f30a75947';
+CLIENT_SECRET = '4677872c4c7ac6c19ffa305fea1dc8015a592fff2958ff89fa5ae55bbb56dbf3';
+
 var app = express();
 app.use(express.static('public'));
 
