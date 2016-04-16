58f6e3e5179dee62c336b10051e29c4a9f7ed43b
Will Moritz <will@crowdfavorite.com>
2016-04-16 13:31:19 -0400

    fuck

--- main.js
+++ main.js
@@ -1,13 +1,53 @@
 var express = require('express');
-
-CLIENT_ID = 'amzn1.application-oa2-client.b4410251e2674ae09b17315f30a75947';
-CLIENT_SECRET = '4677872c4c7ac6c19ffa305fea1dc8015a592fff2958ff89fa5ae55bbb56dbf3';
+var http = require('http');
+var querystring = require('querystring');
 
 var app = express();
 app.use(express.static('public'));
 
-app.get('/', function(req, res) {
-    res.render(index);
+var CLIENT_ID = 'amzn1.application-oa2-client.b4410251e2674ae09b17315f30a75947';
+var CLIENT_SECRET = '4677872c4c7ac6c19ffa305fea1dc8015a592fff2958ff89fa5ae55bbb56dbf3';
+var ACCESS_TOKEN = "figuremeout";
+
+var options = {
+  host: 'api.audible.com',
+  path: '/1.0/content/B0099RKRTY/licenserequest',
+  method: 'POST',
+  port: 80,
+  headers: {
+    'Authorization': ('bearer ' + ACCESS_TOKEN),
+    'Client-ID': CLIENT_ID,
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Content-Length': Buffer.byteLength(data),
+    'Accept' : 'application/json'
+  }
+}
+
+var data = querystring.stringify({
+  "Consumption_type":"Streaming",
+  "Drm_type":"Hls"
 });
+
+var req = http.request(options, function(res){
+  res.on('data', function(chunk){
+    console.log('data');
+  });
+}).on("error", function(e){
+  console.log("Got error: " + JSON.stringify(e ));
+});
+
+req.write(data);
+req.end();
+
+
+app.get('/', function(req, res) {
+  res.render(index);
+});
+
+
+app.get('/auth', function(req, res) {
+  console.log(req.query.access_token)
+});
+
 
 app.listen(process.env.PORT || 8080);
--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,33 @@
+var request = {
+  ACCESS_TOKEN: 'FIGURE_ME_OUT',
+  CLIENT_ID: 'amzn1.application-oa2-client.b4410251e2674ae09b17315f30a75947',
+  url: 'https://api.audible.com/1.0/content/B0099RKRTY/licenserequest',
+  data: {
+    "Consumption_type":"Streaming",
+    "Drm_type":"Hls"
+  }
+}
+
+// access token
+
 $(document).ready(function() {
-  alert("hello oddibble!");
+
+  $.ajax({
+    url: request.url,
+    type: 'POST',
+    dataType: 'json',
+    data: request.data,
+    headers: {
+      'Authorization': ('bearer ' + request.ACCESS_TOKEN),
+      'Client-ID': request.CLIENT_ID,
+      'Accept' : 'application/json'
+    }
+  })
+  .done(function(data) {
+    console.log("success", data);
+  })
+  .fail(function(e) {
+    console.log("error:", e.statusText);
+  });
+
 });
